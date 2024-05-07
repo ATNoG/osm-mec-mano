fddd8a4885f36fc6eefafdf7598b6a3e79c36209
MagalhaesD77 <97603959+MagalhaesD77@users.noreply.github.com>
2024-05-07 12:51:02 +0100

    Update dev_lcm.sh
    I am so dumb...

--- dev_lcm.sh
+++ dev_lcm.sh
@@ -10,28 +10,6 @@ function build {
     echo "Building LCM"
     docker build -t $DOCKER_REPOSITORY/lcm:$VERSION -f devops/Dockerfile.lcm .
     docker push $DOCKER_REPOSITORY/lcm:$VERSION
-    docker rmi $DOCKER_REPOSITORY/lcm:$VERSION
-}
-
-function apply {
-    # Update the OSM with the new LCM
-    echo "Updating OSM with the new LCM"
-    kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "Always"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "'$DOCKER_REPOSITORY'/lcm:'$VERSION'"}]'
-}
-
-function undo {
-    #!/bin/bash
-
-# This script is used to build the LCM library and use it in OSM
-
-DOCKER_REPOSITORY="magalhaesdit"
-VERSION="latest"
-
-function build {
-    # Build the LCM image and push it to the repository
-    echo "Building LCM"
-    docker build -t $DOCKER_REPOSITORY/lcm:$VERSION -f devops/Dockerfile.lcm .
-    docker push $DOCKER_REPOSITORY/lcm:$VERSION
     # docker rmi $DOCKER_REPOSITORY/lcm:$VERSION
 }
 
@@ -44,24 +22,6 @@ function apply {
 function undo {
     # Use the default LCM image
     kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "IfNotPresent"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "opensourcemano/lcm:15"}]'
-}
-
-
-if [ "$1" == "apply" ]; then
-    if [ -z "$2" ]; then
-        apply
-    elif [ "$2" == "build" ]; then
-        build
-        apply
-    else
-        echo "Invalid argument. Please provide 'apply' or 'apply build'."
-    fi
-    
-elif [ "$1" == "undo" ]; then
-    undo
-else
-    echo "Invalid argument. Please provide 'apply' or 'undo'."
-fi
 }
 
 
