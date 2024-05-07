7b923b7f6d7d0fa294bb6115240c739f39bd818b
Ubuntu <ubuntu@osm-dev-d.magalhaes>
2024-05-07 11:50:48 +0100

    Build could not be called

--- dev_lcm.sh
+++ dev_lcm.sh
@@ -6,6 +6,7 @@ DOCKER_REPOSITORY="magalhaesdit"
 VERSION="latest"
 
 function build {
+    # Build the LCM image and push it to the repository
     echo "Building LCM"
     docker build -t $DOCKER_REPOSITORY/lcm:$VERSION -f devops/Dockerfile.lcm .
     docker push $DOCKER_REPOSITORY/lcm:$VERSION
@@ -13,26 +14,27 @@ function build {
 }
 
 function apply {
-
-    if [ "$2" == "build" ]; then
-        build
-    fi
-
     # Update the OSM with the new LCM
     echo "Updating OSM with the new LCM"
     kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "Always"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "'$DOCKER_REPOSITORY'/lcm:'$VERSION'"}]'
-
 }
 
 function undo {
-
     # Rollout undo 1 step
     kubectl rollout undo -n osm deployment/lcm
 }
 
 
 if [ "$1" == "apply" ]; then
-    apply
+    if [ -z "$2" ]; then
+        apply
+    elif [ "$2" == "build" ]; then
+        build
+        apply
+    else
+        echo "Invalid argument. Please provide 'apply' or 'apply build'."
+    fi
+    
 elif [ "$1" == "undo" ]; then
     undo
 else
