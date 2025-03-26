2dc49e5e1e9bcd5b8ede5cc56b77a78a958def9b
Diogo Magalhães <d.magalhaes@ua.pt>
2025-03-26 09:53:46 +0000

    Add IM and update development script

---
+++ IM/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+import sys
+import os
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class Install_osm_im(install):
+    """Generation of .py files from yang models"""
+    model_dir = "models/yang"
+    im_dir = "osm_im"
+
+    def pipinstall(self, package):
+        """pip install for executable dependencies"""
+        subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+    def run(self):
+        self.pipinstall('pyang==2.6.1')
+        self.pipinstall('pyangbind==0.8.6')
+        import pyangbind
+        print("Using dir {}/{} for python artifacts".format(os.getcwd(), self.im_dir))
+        path = "{}/{}".format(os.getcwd(), self.im_dir)
+
+        protoc_command = ["make", "models"]
+        if subprocess.call(protoc_command) != 0:
+            sys.exit(-1)
+
+        # To ensure generated files are copied to the python installation folder
+        install_path = "{}{}".format(self.install_lib, self.im_dir)
+        self.copy_tree(self.im_dir, install_path)
+        if os.path.isfile("{}/etsi-nfv-nsd.py".format(install_path)):
+            self.move_file(
+                "{}/etsi-nfv-nsd.py".format(install_path), "{}/etsi_nfv_nsd.py".format(install_path)
+            )
+        if os.path.isfile("{}/etsi-nfv-vnfd.py".format(install_path)):
+            self.move_file(
+                "{}/etsi-nfv-vnfd.py".format(install_path), "{}/etsi_nfv_vnfd.py".format(install_path)
+            )
+
+        install.run(self)
+
+
+setup(
+    name='osm_im',
+    description='OSM Information Model',
+    long_description=open('README.rst').read(),
+    version_command=('git describe --tags --long --dirty --match v*', 'pep440-git-full'),
+    author='OSM Support',
+    author_email='osmsupport@etsi.org',
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=['setuptools-version-command'],
+    test_suite='nose.collector',
+    url='https://osm.etsi.org/gitweb/?p=osm/IM.git;a=summary',
+    license='Apache 2.0',
+    cmdclass={'install': Install_osm_im},
+)
--- scripts/dev_lcm.sh
+++ scripts/dev_lcm.sh
@@ -9,29 +9,40 @@ DIR="$(pwd)"
 function build {
     # Build the LCM image and push it to the repository
     scripts/local-build.sh --no-cache --run-httpserver
-    scripts/local-build.sh --no-cache --module common,LCM stage-2
-    scripts/local-build.sh --no-cache --module LCM stage-3
-    scripts/local-build.sh --no-cache --module LCM registry-push
+    scripts/local-build.sh --no-cache --module common,IM,LCM,NBI stage-2
+    scripts/local-build.sh --no-cache --module LCM,NBI stage-3
+    scripts/local-build.sh --no-cache --module LCM,NBI registry-push
 }
 
 function apply {
     # Update the OSM with the new LCM
     echo "Updating OSM with the new LCM"
-    apply_status=$(kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "Always"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "'$DOCKER_REPOSITORY'/lcm:'$VERSION'"}]')
+    lcm_apply_status=$(kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "Always"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "'$DOCKER_REPOSITORY'/lcm:'$VERSION'"}]')
+    nbi_apply_status=$(kubectl patch deployment -n osm nbi --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "Always"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "'$DOCKER_REPOSITORY'/nbi:'$VERSION'"}]')
 
     # If already up to date, restart the LCM pod to apply the changes
-    if [ "$apply_status" == "deployment.apps/lcm patched" ]; then
+    if [ "$lcm_apply_status" == "deployment.apps/lcm patched" ]; then
         echo "LCM updated successfully"
     else
         # Restart the LCM pod
         echo "Image already up to date. Restarting the LCM pod to apply the changes"
         kubectl rollout restart -n osm deployment/lcm
     fi
+
+    # If already up to date, restart the NBI pod to apply the changes
+    if [ "$nbi_apply_status" == "deployment.apps/nbi patched" ]; then
+        echo "NBI updated successfully"
+    else
+        # Restart the NBI pod
+        echo "Image already up to date. Restarting the NBI pod to apply the changes"
+        kubectl rollout restart -n osm deployment/nbi
+    fi
 }
 
 function undo {
     # Use the default LCM image
     kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "IfNotPresent"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "opensourcemano/lcm:15"}]'
+    kubectl patch deployment -n osm nbi --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "IfNotPresent"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "opensourcemano/nbi:15"}]'
 }
 
 
