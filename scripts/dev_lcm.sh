8c6abc1fab1ad0433009c7143077396b654fcf3f
Diogo Magalhães <d.magalhaes@ua.pt>
2025-04-10 21:01:09 +0000

    update scripts for way faster build

--- scripts/dev_lcm.sh
+++ scripts/dev_lcm.sh
@@ -4,7 +4,6 @@
 
 DOCKER_REPOSITORY="localhost:5000/opensourcemano"
 VERSION="devel"
-DIR="$(pwd)"
 
 function build {
     # Build the LCM image and push it to the repository
@@ -39,10 +38,20 @@ function apply {
     fi
 }
 
+function update {
+    # Update the code inside the images
+    docker build -f scripts/docker/Dockerfile.LCM -t $DOCKER_REPOSITORY/lcm:$VERSION .
+    docker build -f scripts/docker/Dockerfile.NBI -t $DOCKER_REPOSITORY/nbi:$VERSION .
+
+    # Push the updated image to the repository
+    docker push $DOCKER_REPOSITORY/lcm:$VERSION
+    docker push $DOCKER_REPOSITORY/nbi:$VERSION
+}
+
 function undo {
     # Use the default LCM image
-    kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "IfNotPresent"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "opensourcemano/lcm:15"}]'
-    kubectl patch deployment -n osm nbi --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "IfNotPresent"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "opensourcemano/nbi:15"}]'
+    kubectl patch deployment -n osm lcm --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "IfNotPresent"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "opensourcemano/lcm:17"}]'
+    kubectl patch deployment -n osm nbi --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "IfNotPresent"}, {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "opensourcemano/nbi:17"}]'
 }
 
 
@@ -57,8 +66,17 @@ elif [ "$1" == "build" ]; then
     else
         echo "Invalid argument. Please provide 'build' without any additional arguments."
     fi
+elif [ "$1" == "update" ]; then
+    if [ -z "$2" ]; then
+        update
+    elif [ "$2" == "apply" ]; then
+        update
+        apply
+    else
+        echo "Invalid argument. Please provide 'build' without any additional arguments."
+    fi
 elif [ "$1" == "undo" ]; then
     undo
 else
-    echo "Invalid argument. Please provide 'build', 'apply', 'build apply' or 'undo'."
+    echo "Invalid argument. Please provide 'build', 'update, 'apply', 'build apply', 'update apply' or 'undo'."
 fi
